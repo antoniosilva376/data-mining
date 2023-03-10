58e833d28f4f1f8f43baedd8b54813386e5ee54a
antoniosilva376 <tugasdonorte@gmail.com>
2023-03-10 16:31:45 +0000

    dataset missing one method

--- aula1/dataset.py
+++ aula1/dataset.py
@@ -1,13 +1,11 @@
-
-import csv
 import numpy as np
 
 class Dataset:
-    #use y= anything for it to be created
-    def __init__(self, X=None, y=None, column_names=None):
+    def __init__(self, X=None, y=None, column_names=None, label=None):
         self.X = X
         self.y = y
         self.column_names = column_names
+        self.label = label
         
     def set_X(self, X):
         self.X = X
@@ -17,6 +15,9 @@ class Dataset:
         
     def set_column_names(self, column_names):
         self.column_names = column_names
+
+    def set_label(self, label):
+        self.label=label
         
     def get_X(self):
         return self.X
@@ -26,27 +27,97 @@ class Dataset:
     
     def get_column_names(self):
         return self.column_names
+
+    def get_label(self):
+        return self.label
             
-    def read_csv(self, filename, sep=','):
-        with open(filename, 'r') as f:
-            lines = f.readlines()
-            data = [line.strip().split(',') for line in lines[1:]]
-        if self.y is None:
-            self.column_names = lines[0].strip().split(',')
-            self.X = np.array(data)
-        else:
-            self.column_names = lines[0].strip().split(',')[:-1]
-            self.X = np.array(data)[:, :-1]
-            self.y = np.array(data)[:, -1]
+    def read_csv(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                lines = f.readlines()
+                data = [line.strip().split(',') for line in lines[1:]]
+                self.column_names = lines[0].strip().split(',')
+            if self.y is None:
+                self.X = np.array(data)
+            else:
+                self.X = np.array(data)[:, :-1]
+                self.y = np.array(data)[:, -1]
+        except FileNotFoundError:
+            print(f'file not found')
+
+    def write_csv(self, filename):
+        try:
+            with open(filename, 'w') as f:
+                f.write(','.join(self.features_names) + ',' + self.label + '\n')
+                for row in range(self.X.shape[0]):
+                    f.write(','.join([str(elem) for elem in self.X[row]]) + ',' + str(self.y[row]) + '\n')
+        except IOError:
+            print(f'error writing csv')
+
+    def read_tsv(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                lines = f.readlines()
+                data = [line.strip().split('\t') for line in lines[1:]]
+                self.column_names = lines[0].strip().split('\t')
+            if self.y is None:
+                self.X = np.array(data)
+            else:
+                self.X = np.array(data)[:, :-1]
+                self.y = np.array(data)[:, -1]
+        except FileNotFoundError:
+            print(f'file not found')
+
+    def write_tsv(self, filename):
+        try:
+            with open(filename, 'w') as f:
+                header = '\t'.join(['col' + str(i) for i in range(self.X.shape[1])])
+                header += '\tlabel\n'
+                f.write(header)
+                for row in range(self.X.shape[0]):
+                    row_values = '\t'.join([str(elem) for elem in self.X[row]])
+                    row_values += '\t' + str(self.y[row]) + '\n'
+                    f.write(row_values)
+        except IOError:
+            print(f"error writing tsv")
+
         
     def count_nulls(self):
         return np.sum(self.X == '', axis=0)
           
+    #needs median version
     def fill_nulls(self):
-        pass
+        X2 = self.X[:, 1:]
+        X2[X2 == ''] = np.nan
+        means = np.nanmean(X2.astype(float), axis=1)
+        for i in range(self.X.shape[0]):
+            for j in range(self.X.shape[1]):
+                if self.X[i][j] == 'nan':
+                    self.X[i][j] = means[i]
 
     def describe(self):
-        pass
+        num_elements = self.X.shape[0]
+        means = np.mean(self.X[:, 1:].astype(float), axis=0)
+        stds = np.std(self.X[:, 1:].astype(float), axis=0)
+        mins = np.min(self.X[:, 1:].astype(float), axis=0)
+        maxs = np.max(self.X[:, 1:].astype(float), axis=0)
+        q25 = np.percentile(self.X[:, 1:].astype(float), 25, axis=0)
+        q50 = np.percentile(self.X[:, 1:].astype(float), 50, axis=0)
+        q75 = np.percentile(self.X[:, 1:].astype(float), 75, axis=0)
+
+        for i, column_name in enumerate(self.column_names[1:]):
+            if column_name != "":
+                print("--------------------------------\n")
+                print("column:", column_name)
+                print("Number of elements:", num_elements)
+                print("Minimum value:", mins[i])
+                print("Maximum value: ", maxs[i])
+                print("Mean:", means[i])
+                print("Standard deviation:", stds[i])
+                print("25th percentile:", q25[i])
+                print("50th percentile (median):", q50[i])
+                print("75th percentile:", q75[i])
+                print("--------------------------------\n")
 
 
 
@@ -63,4 +134,5 @@ print("y:\n" + str(ds.y))
 print("column_names:\n" + str(ds.column_names))
 print("count_nulls:\n" + str(ds.count_nulls()))
 ds.describe()
+
 
