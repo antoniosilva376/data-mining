94880cb3e831481996d08939a70240eed66f3fab
antoniosilva376 <pg47033@alunos.uminho.pt>
2023-05-29 19:59:17 +0100

    Aula2 test class refactoring

--- aula2/f_classif.py
+++ aula2/f_classif.py
@@ -1,8 +1,10 @@
 import numpy as np
 import sys
+from scipy.stats import f_oneway
+from sklearn.feature_selection import f_classif as sk_f_classif
+import unittest
 sys.path.append('..')
 from aula1.dataset import Dataset
-from scipy.stats import f_oneway
 
 class f_classif:
     def __init__(self, dataset: Dataset):
@@ -15,6 +17,28 @@ class f_classif:
         print(p)
         return F, p
 
+
+class TestFClassif(unittest.TestCase):
+    def test_f_classif(self):
+        # create a toy dataset
+        X = np.random.randn(100, 5)
+        y = np.random.choice(["A", "B", "C"], 100)
+        dataset = Dataset(X=X, y=y)
+
+        # calculate the one-way ANOVA using the f_classif class
+        f = f_classif(dataset)
+        Fv, pv = f.calculate()
+
+        # calculate the one-way ANOVA using scikit-learn's f_classif function
+        Fv_sk, pv_sk = sk_f_classif(X, y)
+
+        # compare the results
+        np.testing.assert_allclose(Fv, Fv_sk)
+        np.testing.assert_allclose(pv, pv_sk)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 """
 # create a toy dataset
--- aula2/f_regression.py
+++ aula2/f_regression.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 from scipy.stats import f
+import unittest
+from sklearn.feature_selection import f_regression as sk_f_regression
 
 sys.path.append('..')
 from aula1.dataset import Dataset
@@ -14,40 +16,50 @@ class f_regression:
         X = self.dataset.X
         y = self.dataset.y
 
-        # add a column of ones to X to account for the intercept term
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        # center the data
+        X = X - X.mean(axis=0)
+        y = y - y.mean()
 
-        # calculate the coefficients of the linear regression model
-        beta_hat = np.linalg.inv(X.T @ X) @ X.T @ y
+        # calculate the correlation between each feature and the target
+        corr = np.dot(X.T, y) / np.sqrt(np.dot(y.T, y) * np.sum(X ** 2, axis=0))
 
-        # calculate the residual sum of squares
-        e = y - X @ beta_hat
-        RSS = e.T @ e
-
-        # calculate the total sum of squares
-        y_bar = y.mean()
-        TSS = ((y - y_bar) ** 2).sum()
-
-        # calculate the degrees of freedom for the numerator and denominator
-        df_num = X.shape[1] - 1
-        df_denom = X.shape[0] - X.shape[1]
+        # calculate the degrees of freedom
+        degrees_of_freedom = X.shape[0] - 2
 
         # calculate the F-statistic for each feature
-        F = np.zeros(X.shape[1])
-        for i in range(X.shape[1]):
-            X_i = X[:, i]
-            beta_i = beta_hat[i]
-            e_i = y - beta_i * X_i
-            RSS_i = e_i.T @ e_i
-            F[i] = ((TSS - RSS_i) / df_num) / (RSS_i / df_denom)
+        F = corr ** 2 / (1 - corr ** 2) * degrees_of_freedom
 
         # calculate the p-values for each feature
-        p_values = [1 - f.cdf(f_stat, df_num, df_denom) for f_stat in F]
+        p_values = f.sf(F, 1, degrees_of_freedom)
+
+        return F, p_values
 
 
         print(p_values[1:])
         return None, p_values[1:]  # exclude the intercept term
 
+
+class TestFRegression(unittest.TestCase):
+    def test_f_regression(self):
+        # create a toy dataset
+        X = np.random.randn(100, 5)
+        y = np.random.randn(100)
+        dataset = Dataset(X=X, y=y)
+
+        # perform linear regression using the f_regression class
+        f = f_regression(dataset)
+        Fv, pv = f.calculate()
+
+        # perform linear regression using scikit-learn's f_regression function
+        Fv_sk, pv_sk = sk_f_regression(X, y)
+
+        # compare the results
+        np.testing.assert_allclose(Fv, Fv_sk)
+        np.testing.assert_allclose(pv, pv_sk)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 """
 # create a toy dataset
@@ -62,4 +74,3 @@ pv = f1.calculate()
 
 print("p-values:", pv)
 """
-
